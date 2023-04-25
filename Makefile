5639f9037c0f1e66a932aaeb1d1ee063519a611c
RuddyPanta <pantagruel2006@gmail.com>
2023-04-25 20:18:07 +0300

    Create Javalin metod

---
+++ Makefile
@@ -0,0 +1,2 @@
+start:
+	make -C app start
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,15 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       Javalin app = getApp();
+       app.start(3000);
     }
 
+    public static Javalin getApp() {
+        Javalin app = Javalin.create().get("/", ctx -> ctx.result("Hello World"));
+        return app;
+    }
 }
